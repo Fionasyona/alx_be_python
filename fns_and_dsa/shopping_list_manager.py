740c14ca2e34bc05faed3b9abb5ca8c399951bf0
Fiona Wambua <fionawambua20@gmail.com>
2025-06-05 18:37:21 +0300

    commit new changes

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,30 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item = ("Enter an item to buy: ")
+            pass
+        elif choice == '2':
+            item = ("Enter an item to remove: ")
+            pass
+        elif choice == '3':
+            print("The shopping list: ")
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
