277c8f4cafcde16f2c93da7cdcbf21b3e553d6db
Fiona Wambua <fionawambua20@gmail.com>
2025-06-13 16:21:28 +0300

    commit all changes

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,7 +7,7 @@ class TestSimpleCalculator(unittest.TestCase):
 
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, -2), -3)
