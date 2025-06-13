2097d6bbe91a07ddc3df23f1dd89a1d47f503d57
Fiona Wambua <fionawambua20@gmail.com>
2025-06-13 16:23:25 +0300

    all changes

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,29 +12,24 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, -2), -3)
         self.assertEqual(self.calc.add(-1, 2), 1)
-        self.assertEqual(self.calc.add(0, 0), 0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(3, 5), -2)
         self.assertEqual(self.calc.subtract(0, 0), 0)
-        self.assertEqual(self.calc.subtract(-1, -1), 0)
 
     def test_multiply(self):
     
         self.assertEqual(self.calc.multiply(4, 5), 20)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
         self.assertEqual(self.calc.multiply(0, 10), 0)
-        self.assertEqual(self.calc.multiply(-4, -2), 8)
-
+    
     def test_divide(self):
     
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(5, 2), 2.5)
         self.assertEqual(self.calc.divide(-6, 3), -2)
-        self.assertEqual(self.calc.divide(0, 5), 0)
-        self.assertIsNone(self.calc.divide(5, 0))  # Division by zero
-
+        
 if __name__ == '__main__':
     unittest.main()
